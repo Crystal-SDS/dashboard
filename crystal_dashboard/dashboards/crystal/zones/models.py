2ab92588ee82651be95d1327a09105321e4723b4
JoanBoronat <joan.boronatr@estudiants.urv.cat>
2017-09-21 12:07:17 +0200

    Zone models

---
+++ crystal_dashboard/dashboards/crystal/zones/models.py
@@ -0,0 +1,10 @@
+
+class Zone:
+    """
+        ProxyNode class defines a Swift Proxy node. The identifier is the name of the node.
+    """
+    def __init__(self, id_zone, name, description):
+        self.id = id_zone
+        self.name = name
+        self.description = description
+
