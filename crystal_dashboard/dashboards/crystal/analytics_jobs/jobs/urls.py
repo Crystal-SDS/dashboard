845f158ee98d810f90ec0f58f3ba213b9463a822
gerardparis <gerard.paris@gmail.com>
2017-09-29 14:02:28 +0200

    Fixed urlpatterns (2)

--- crystal_dashboard/dashboards/crystal/analytics_jobs/jobs/urls.py
+++ crystal_dashboard/dashboards/crystal/analytics_jobs/jobs/urls.py
@@ -1,8 +1,7 @@
-from django.conf.urls import patterns
 from django.conf.urls import url
 
 from crystal_dashboard.dashboards.crystal.analytics_jobs.jobs import views
 
-urlpatterns = patterns(
+urlpatterns = [
     url(r'^submit_job/$', views.SubmitJobView.as_view(), name='submit_job'),
-)
+]
